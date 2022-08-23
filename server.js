75daf34f737fe193cd27e71abbc71006cd9c10d1
jafartafary <jafartafary@gmail.com>
2022-08-23 21:48:49 +0300

    Fix server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express') ;
 const dotenv = require('dotenv')
-const http = require('http');
 
 
 dotenv.config();
 const app = express();
 const PORT = 3000;
 
-http.createServer(app);
 app.use(express.static(`${__dirname}/dist`));
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT ||process.env.PORT, () => {
     console.log(`Express app listening on port ${PORT}`);
 });
